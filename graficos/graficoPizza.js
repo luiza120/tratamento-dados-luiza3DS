aca2b5d6b2176f91b8eaee02001216ce6c801245
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-10-03 12:12:57 -0300

    att

--- graficos/graficoPizza.js
+++ graficos/graficoPizza.js
@@ -1,12 +1,35 @@
+import { pegarCSS } from "./comum.js"
+
 async function criarGraficoPizza(){
     const url = 'https://raw.githubusercontent.com/luiza120/2024-API-FDB-LUIZA3DS.JSON/refs/heads/main/graficobarra.json'
     const res = await fetch(url)
     const dados = await res.json()
+    const maquiagensVendidas = Object.keys(dados)
+    const maquiagensUsos = Object.values(dados)
 
-    for( let i = 0; i<= dados.length; i++){
-        let listaNome = []
-
-        listaNome.push
+    const data = [
+        {
+            values: maquiagensUsos,
+            labels: maquiagensVendidas,
+            type: 'pie',
+            textinfo: 'label + percent'
+        }
+    ]
+    const layout = {
+        height: 400,
+        width: 600,
+        plot_color: pegarCSS('--vermelho'),
+        paper_bgcolor: pegarCSS('--preto')
     }
-    console.log(dados[i].nome)
+
+    const maquiagensTitulo = document.createElement('h3')
+    maquiagensTitulo.classList.add('caixa-grafico_titulo')
+    maquiagensTitulo.innerHTML = `As <span>Maquiagens mais Usadas</span> no Brasil`
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico-disco'
+    document.getElementById('caixa-grafico').appendChild(maquiagensTitulo)
+    document.getElementById('caixa-grafico').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
 }
+criarGraficoPizza()
