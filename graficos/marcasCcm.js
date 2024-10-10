c701caf8a4a1a0829e7a1f281ce50f4ae554c7f8
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-10-10 12:17:29 -0300

    Add files via upload

--- graficos/marcasCcm.js
+++ graficos/marcasCcm.js
@@ -1,4 +1,4 @@
-import { pegarCSS } from "./comum"
+import { pegarCSS } from "./comum.js"
 
 async function graficosCcm(){
     const url = 'https://raw.githubusercontent.com/luiza120/2024-API-FDB-LUIZA3DS.JSON/refs/heads/main/pesquisaCcm.json'
@@ -6,33 +6,33 @@ async function graficosCcm(){
     const dados = await res.json()
     const marcasVotadas = dados.slice(1).map(marcas => marcas[2])
     const contagemVotagem = marcasVotadas.reduce((acc, marcasVotadas) => {
-        acc[marcasVotadas] = (acc,[marcasVotadas] || 0)+ 1
+        acc[marcasVotadas] = (acc, [marcasVotadas] || 0) + 1
         return acc
     }, {})
 
     const valores = Object.values(contagemVotagem)
-    const etiqueta = OBject.keys(contagemVotagem)
+    const etiqueta = Object.keys(marcasVotadas)
 
     const data = [
         {
             values: valores,
             labels: etiqueta,
             type: 'pie',
-            tetxoinfo: 'label+percent'
+            tetxoinfo: 'label+percent',
         }
     ]
     const layout = 
     {
-        plot_bgcolor: pegarCSS('--salmao'),
+        plot_bgcolor: pegarCSS('--branco'),
         paper_bgcolor: pegarCSS('--vermelho'),
         font:{
-            color: pegarCSS(--salmao),
+            color: pegarCSS('--preto'),
             family: pegarCSS('---fonte-primaria'),
             size: 16,
         }
     }
     const pesquisaCcm = document.createElement('h3')
-    pesquisaCcm.classList.add('caixa-grafico_titulo')
+    pesquisaCcm.classList.add('caixa-grafico__titulo')
     pesquisaCcm.innerHTML = `As marcas mais votadas<span>CCM</span>`
 
     const grafico = document.createElement('div')
@@ -43,12 +43,13 @@ async function graficosCcm(){
         responsive: true,
         displeyModeBar: false 
     }
-    Plotly.newpLot(grafico, data, layout)
+    Plotly.newPlot(grafico, data, layout)
 
-    const caixa = document.getElementById(caixa-grafico)
+    const caixa = document.getElementById('caixa-grafico')
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('caixa-grafico__texto')
     paragrafo.innerHTML = 'Nota-se que a marca mais votada na escola CCM é diferente do mais votada no mundo. Enquanto os estudantes elegeram, com 16 votos, <span>a O boticário</span> como a marca favorita, já a pesquisa global indicou que <span>a Natura</span> é a marca mais comprada no mundo. Na pesquisa feita na escola, a mesma ficou em quarto lugar'
     caixa.appendChild(paragrafo)
 }
+
 graficosCcm()
